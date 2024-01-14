8e8c7f2d7ad676155e950237e26da9ab75f27dea
Sergiy-Shevchenko <sergiy_shevchenko@i.ua>
2024-01-14 22:26:35 +0200

    fixed lcontact ink

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,17 +4,16 @@ export const Contact = () => {
   return (
     <div className={css.section}>
       <div className={css.contact__phone}>
-        <a className={css.link__phone} href="tell:+380976928436">
+        <a className={css.link__phone} href="tel:+380976928436">
           <Icon className={css.icon__phone} id="icon-phone" />
           <p className={css.phone}>+380976928436</p>
         </a>
-        <a className={css.link__phone} href="tell:+380992460386">
+        <a className={css.link__phone} href="tel:+380992460386">
           <Icon className={css.icon__phone} id="icon-phone" />
           <p className={css.phone}>+380992460386</p>
         </a>
       </div>
-
-      <a className={css.link__email} href="iovsiichuknotar@gmail.com">
+      <a className={css.link__email} href="mailto:iovsiichuknotar@gmail.com">
         <Icon className={css.icon__email} id="icon-mail" />
         <p className={css.email}>iovsiichuknotar@gmail.com</p>
       </a>

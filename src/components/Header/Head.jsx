2bee3ac35099459a324d14624099a46230f6ddb4
Sergiy-Shevchenko <sergiy_shevchenko@i.ua>
2024-01-18 20:44:35 +0200

    add conponent Name

--- src/components/Header/Head.jsx
+++ src/components/Header/Head.jsx
@@ -3,19 +3,25 @@ import img from '../../image/tryzub.svg';
 import logo from '../../image/logo-kyiv.png';
 import css from './Head.module.css';
 
+const Name = () => {
+  return (
+     <div className={css.section__name}>
+            <h1 className={css.name}>ОВСІЙЧУК </h1>
+            <h2 className={css.name}> Ірина Григорівна</h2>
+          </div>
+  )
+}
+
 export const Head = () => {
   return (
     <div className={css.head}>
 <div className={css.container}>
       <img className={css.picture} src={img} alt="gerb" />
       <div className={css.section}>
-        <div className={css.header__title}>
+        <div className={css.title}>
           <h3 className={css.city}>КИЇВСЬКИЙ МІСЬКИЙ НОТАРІАЛЬНИЙ ОКРУГ</h3>
           <h2 className={css.job}>НОТАРІУС</h2>
-          <span className={css.section__name}>
-            <h1 className={css.name}>ОВСІЙЧУК </h1>
-            <h2 className={css.name}> Ірина Григорівна</h2>
-          </span>
+         <Name/>
           <h4 className={css.certificate}>Свідоцтво 9690</h4>
         </div>
         <Contact />

2c339d3c037699b0829ad91922f352b6be05f279
Sergiy-Shevchenko <sergiy_shevchenko@i.ua>
2024-01-18 20:21:26 +0200

    1

--- src/components/Header/Head.jsx
+++ src/components/Header/Head.jsx
@@ -9,7 +9,7 @@ export const Head = () => {
 <div className={css.container}>
       <img className={css.picture} src={img} alt="gerb" />
       <div className={css.section}>
-        <span className={css.header__title}>
+        <div className={css.header__title}>
           <h3 className={css.city}>КИЇВСЬКИЙ МІСЬКИЙ НОТАРІАЛЬНИЙ ОКРУГ</h3>
           <h2 className={css.job}>НОТАРІУС</h2>
           <span className={css.section__name}>
@@ -17,7 +17,7 @@ export const Head = () => {
             <h2 className={css.name}> Ірина Григорівна</h2>
           </span>
           <h4 className={css.certificate}>Свідоцтво 9690</h4>
-        </span>
+        </div>
         <Contact />
       </div>
       <img className={css.logo} src={logo} alt="logo" />

105418a5cea938e63fdfcf2688abe48561d43264
Sergiy-Shevchenko <sergiy_shevchenko@i.ua>
2024-01-18 15:16:41 +0200

    styles fixed

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,3 +1,5 @@
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import Notiflix from 'notiflix'
 import { Head } from "components/Header/Head";
 import { Hero } from "components/Hero/Hero";
 import { Servise } from "components/Service/Servise";
@@ -8,11 +10,27 @@ import { Shedule } from "components/Schedule/Schedule";
 import css from './Main.module.css'
 import { LinkComtact } from "components/Link/LinkContact";
 
+
+  Notiflix.Notify.success(
+    'м.Київ, вул.Саксаганського, буд 119, Бізнес центр "Botanic Tower", офіс №13',
+    {
+      position: 'right-top',
+      timeout: 5000,
+      fontSize: '16px',
+      cssAnimationDuration: 1500,
+      distance: '30px',
+      cssAnimationStyle: 'from-right',
+      fontAwesomeIconStyle: 'shadow',
+    }, );
+
+
 export const Main = () => {
+  
+ 
     return (
         <div className={css.container}>
             <div className={css.bg_ios_back}>
-            <Head/>
+                       <Head/>
             <LinkComtact/>
             <Hero/>
            <Servise/>
